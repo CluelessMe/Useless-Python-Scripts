37ccd03e13a06a4a62281d3a7895b214fa2979ff
CluelessMe <43913418+CluelessMe@users.noreply.github.com>
2020-04-02 20:03:31 -0600

    Create obfuscatedpython.py

---
+++ obfuscatedpython.py
@@ -0,0 +1,11 @@
+# Goal: Make the if statement read true.  Don't run until you know how to make the statement read true.
+# When you do run it, run in Linux.
+_=len,input
+__=lambda len:(((lambda __:__----(_[0](str(_[0]) if not False == False in [False] else str(...))-7))))
+exec = lambda lanbda: _[1](':¢σ∂є: ')
+f = lambda: eval(''.join(chr(ord(c)-1) for c in '\p/gpsl)*!gps!)p-j*!jo!\)``jnqpsu``)(pt(*-!``jnqpsu``)(jufsuppmt(**^!gps!y!jo!j/sfqfbu)1*^'))
+_ = '7530'.__eq__(''.join([chr(ord(c)-__builtins__.__dict__['eval'](str(__(_)(n)))) for n, c in enumerate(exec(_))]))
+if _:
+    print('Great Job!  You deserve a cookie.')
+else:
+    f()
